04ed9859bb8916cd466eed14a10aacc9226c7c06
Kvilleda089 <truj_98@outlook.es>
2024-08-02 13:31:28 -0600

    Mod: se modifica header, para cuando la pantalla sea pequeña c0ntraiga todo en un botón

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -4,25 +4,24 @@ import CommentSection from './CommentSection';
 import '../styles/AboutUsSection.css'
 const AboutUsSection: React.FC = () => {
 
-    return (
-        <div className="container-about-us">
-          <div className="relative w-11/12 mx-auto div-information">
-      <div className="container-information">
-        <h1 className="text-2xl font-bold mb-2">Who We Are</h1>
-        <p className="mb-1 text-gray-500">Problems trying to resolve the conflict between</p>
-        <p className="mb-1 text-gray-500">the two major realms of Classical physics: Newtonian mechanics</p>
-      </div>
-      <div className="container-image mt-4">
-        <img src={imageWhoWeAre} alt="Presentation Who we are" className="w-1/2 mt-20 mb-20 h-auto mx-auto" />
-      </div>
-      <div className="background-overlay"></div>
-    </div>
-
-            <div className="container-comment-section">
-                <CommentSection />
-            </div>
+  return (
+    <div className="container-about-us w-full p-4">
+      <div className="relative w-full mx-auto div-information">
+        <div className="container-information">
+          <h1 className="text-2xl font-bold mb-2">Who We Are</h1>
+          <p className="mb-1 text-gray-500">Problems trying to resolve the conflict between</p>
+          <p className="mb-1 text-gray-500">the two major realms of Classical physics: Newtonian mechanics</p>
         </div>
-    );
+        <div className="container-image mt-4">
+          <img src={imageWhoWeAre} alt="Presentation Who we are" className="w-1/2 mt-20 mb-20 h-auto mx-auto" />
+        </div>
+        <div className="background-overlay w-full"></div>
+      </div>
+      <div className="container-comment-section">
+        <CommentSection />
+      </div>
+    </div>
+  );
 }
 
 export default AboutUsSection;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import '../styles/Header.css'
+import React, { useState } from 'react';
 import backgroundGray from '../img/fondo-gris.jpg'
 import iconcar from '../img/icon-carrito-compra.png'
 import iconSearch from '../img/icon-search.png'
+import incoMenu from '../img/icon-menu.png';
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header className='header' style={{ backgroundImage: `url(${backgroundGray})`, backgroundPosition: 'right', backgroundSize: 'cover' }}>
-      <h1 >Litigade</h1>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Product</a></li>
-          <li><a href="/">Pricing</a></li>
-          <li><a href="/">Contact</a></li>
-          <li className='ml-20'><a href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10 flex items-center justify-end p-1">
-          <img src={iconSearch} className="w-full h-full" alt="Buscar" /></a></li>
-          <li><a href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10 flex items-center justify-end p-1">
-          <img src={iconcar} className="w-full h-full" alt="Carrito Compras" /></a></li>
-        </ul>
-      </nav>
+      <div className="flex justify-between items-center p-4">
+        <div className='flex ml-32'>
+          <h1 className="flex-grow text-2xl font-bold">Litigade</h1>
+        </div>
+        <button className="block md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <img src={incoMenu} alt="Menu" className="w-6 h-6" />
+        </button>
+        <div className={`${menuOpen ? 'block' : 'hidden'} md:flex md:items-center`}>
+          <nav className="flex flex-col md:flex-row md:items-center md:justify-end mr-30">
+            <ul className="flex flex-col md:flex-row md:items-center md:justify-end">
+              <li className="md:mr-4 text-2xl font-bold"><a href="/">Home</a></li>
+              <li className="md:mr-4 text-2xl font-bold"><a href="/">Product</a></li>
+              <li className="md:mr-4 text-2xl font-bold" ><a href="/">Pricing</a></li>
+              <li className="md:mr-4 text-2xl font-bold"><a href="/">Contact</a></li>
+            </ul>
+          </nav>
+          <div className="relative flex items-center mt-4 md:mt-0 md:ml-4 mr-20">
+            <input
+              type="text"
+              placeholder="Buscar"
+              className="border border-gray-300 rounded-md p-2 pr-10"
+            />
+            <img src={iconSearch} className="w-6 h-6 absolute right-2" alt="Buscar" />
+          </div>
+          <a href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10 flex items-center justify-center mt-4 md:mt-0 md:ml-4 mr-14">
+            <img src={iconcar} className="w-full h-full" alt="Carrito Compras" />
+          </a>
+        </div>
+      </div>
     </header>
   );
 };
